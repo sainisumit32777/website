a625a6571790f6a2e27ee30015f84f487585be33
sainisumit32777 <71331787+sainisumit32777@users.noreply.github.com>
2022-04-18 09:29:44 -0600

    updated

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const navSlide = () =>{
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
     burger.addEventListener('click',() =>{
+        
+        if(nav.style.animation){
+            nav.style.animation = '';
+        }
+        else{
+            nav.style.animation = 'burgerMenuFade 0.5s ease-in';
+        }
         nav.classList.toggle('nav-active');
         navLinks.forEach((link)=>{
             if(link.style.animation){
                 link.style.animation = '';
             }
             else{
-                link.style.animation = 'navLinkFade 0.5s ease forwards 0.2s';
+                link.style.animation = 'navLinkFade 0.6s ease forwards 0.2s';
             }
         });
         burger.classList.toggle('toggle');
